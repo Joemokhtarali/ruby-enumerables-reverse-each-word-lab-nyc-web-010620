daa64e9408a7ffbff31b08357b08fa176c591030
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-26 20:23:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   reversed = []
   words = sentence.split(" ")
   
-   words.each { |word| reversed << word.reverse }
+   words..collect { |word| reversed << word.reverse }
   return reversed.join(" ")
   
 end
